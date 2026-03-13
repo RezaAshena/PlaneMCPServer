c5ac33b9dde40ea159fbcb91fb7c651c9fd23a09
Reza Ashena <Ashena.reza@gmail.com>
2026-03-13 10:56:05 -0400

    Add async project state and work item API methods
    Added GetProjectStatesAsync and CreateworkItemAsync to PlaneApiService for retrieving project states and creating work items via Plane API. Both methods use X-API-Key authentication and return response content as strings. Also, baseUrl is now trimmed for consistent URL formatting.

--- PlaneMCPServer/PlaneApiService.cs
+++ PlaneMCPServer/PlaneApiService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 namespace PlaneMCPServer
 {
@@ -21,10 +22,48 @@ namespace PlaneMCPServer
             )
         {
             _httpClientFactory = httpClientFactory;
-            _baseUrl = baseUrl;
+            _baseUrl = baseUrl.TrimEnd('/');
             _workspace = workspace;
             _projectId = projectId;
             _apiKey = apiKey;
+        }
+
+
+        public async Task<string> GetProjectStatesAsync()
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+
+            httpClient.DefaultRequestHeaders.Add("X-API-Key", _apiKey);
+
+            var url = $"{_baseUrl}/api/v1/workspaces/{_workspace}/projects/{_projectId}/states";
+
+            var response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
+
+        public async Task<string> CreateworkItemAsync(string name, string descriptionHtml, string stateId)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            httpClient.DefaultRequestHeaders.Add("X-API-Key", _apiKey);
+            var url = $"{_baseUrl}/api/v1/workspaces/{_workspace}/projects/{_projectId}/work-items";
+
+            var requestBody = new
+            {
+                name = name,
+                description_html = descriptionHtml,
+                stateId = stateId
+            };
+
+         var JsonContent = JsonSerializer.Serialize(requestBody);
+            var content = new StringContent(JsonContent, Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PostAsync(url, content);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+            return responseContent;
         }
     }
 }
